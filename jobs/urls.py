e64dfa78b249e6960953bb6ae611504c81642227
Vishnu Pramod <vishnupramod24042003@gmail.com>
2025-12-02 00:12:41 +0530

    Completed the project and improved the UI

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import JobListView, JobCreateView
+from .views import JobListView, JobCreateView, JobUpdateView, JobDeleteView, JobDetailView
 
 urlpatterns = [
     path('',JobListView.as_view(),name='dashboard'),
     path('add/',JobCreateView.as_view(),name='add-job'),
+    path('update/<int:pk>/', JobUpdateView.as_view(), name='update-job'),
+    path('delete/<int:pk>/', JobDeleteView.as_view(), name='delete-job'),
+    path('job/<int:pk>/', JobDetailView.as_view(), name='job-detail'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from .models import JobApplication
 from django.urls import reverse_lazy
 
@@ -13,3 +13,19 @@ class JobCreateView(CreateView):
     template_name = 'job_form.html'
     fields = ['company', 'position', 'url', 'status', 'notes']
     success_url = reverse_lazy('dashboard')
+
+class JobUpdateView(UpdateView):
+    model = JobApplication
+    template_name = 'job_form.html'
+    fields = ['company', 'position', 'url', 'status', 'notes']
+    success_url = reverse_lazy('dashboard')
+
+class JobDeleteView(DeleteView):
+    model = JobApplication
+    template_name = 'job_confirm_delete.html'
+    success_url = reverse_lazy('dashboard')
+
+class JobDetailView(DetailView):
+    model = JobApplication
+    template_name = 'job_detail.html'
+    context_object_name = 'job'
